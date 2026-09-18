4e3ca0c76ac8ada0e1261c7b831df365ff17e867
KankiYuma <KankiYuma@192.168.3.136>
2026-09-18 15:18:26 +0900

    add:Case02.java 受講生 ログイン 認証失敗テストコード追加
    ①ログインID、パスワードともに未登録の場合
    ②ログインIDは登録されているがパスワードが一致していない場合
    の確認を行った。
    期待値は以下の通り
    タイトル：ログイン | LMS
    h2タグ：ログイン
    エラーメッセージ：* ログインに失敗しました。
    ログインボタン：ログイン

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +23,12 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース02 受講生 ログイン 認証失敗")
 public class Case02 {
+
+	// ポート番号8080番
+	private final int PORT = 8080;
+
+	/** Driver */
+	private WebDriver driver = WebDriverUtils.webDriver;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,14 +46,61 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:" + PORT + "/lms");
+		// タイトル 一致確認
+		assertEquals("ログイン | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("ログイン", driver.findElement(By.tagName("h2")).getText());
+		// ログインボタン 一致確認
+		assertEquals("ログイン", driver.findElement(By.className("btn-primary")).getAttribute("value"));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02_1() {
+		// ログインIDの入力欄を空白にしておく
+		driver.findElement(By.name("loginId")).clear();
+
+		driver.findElement(By.name("loginId")).sendKeys("WRONG");
+		driver.findElement(By.name("password")).sendKeys("WRONG");
+		driver.findElement(By.className("btn-primary")).click();
+		
+		// タイトル 一致確認
+		assertEquals("ログイン | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("ログイン", driver.findElement(By.tagName("h2")).getText());
+		// エラーメッセージ 一致確認
+		assertEquals("* ログインに失敗しました。", driver.findElement(By.cssSelector(".help-inline.error")).getText());
+		// ログインボタン 一致確認
+		assertEquals("ログイン", driver.findElement(By.className("btn-primary")).getAttribute("value"));
+		getEvidence(new Object() {
+		});
+	}
+
+	@Test
+	@Order(3)
+	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
+	void test02_2() {
+		// ログインIDの入力欄を空白にしておく
+		driver.findElement(By.name("loginId")).clear();
+
+		driver.findElement(By.name("loginId")).sendKeys("StudentAA02");
+		driver.findElement(By.name("password")).sendKeys("StudentAA03");
+		driver.findElement(By.className("btn-primary")).click();
+		
+		// タイトル 一致確認
+		assertEquals("ログイン | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("ログイン", driver.findElement(By.tagName("h2")).getText());
+		// エラーメッセージ 一致確認
+		assertEquals("* ログインに失敗しました。", driver.findElement(By.cssSelector(".help-inline.error")).getText());
+		// ログインボタン 一致確認
+		assertEquals("ログイン", driver.findElement(By.className("btn-primary")).getAttribute("value"));
+		getEvidence(new Object() {
+		});
 	}
 
 }
