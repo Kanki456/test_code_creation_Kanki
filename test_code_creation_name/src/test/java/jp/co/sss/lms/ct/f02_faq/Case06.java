a0e300abc66f8e752327147c70cec1f255e2a3d4
KankiYuma <KankiYuma@192.168.3.136>
2026-09-24 13:36:20 +0900

    add:Case06.java 神吉雄真 カテゴリ検索 正常系テストコード追加
    カテゴリ検索結果の一致確認を行った。
    （質問押下時の回答確認は未実装）
    
    期待値は以下の通り
    
    検索結果1：セルフ・キャリアドック制度とは何か
    検索結果2：事業所が変わった場合、何かしら手続きをする必要がありますか？
    検索結果3：助成金書類の作成方法が分かりません

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case06.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,11 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト よくある質問機能
@@ -18,6 +26,12 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース06 カテゴリ検索 正常系")
 public class Case06 {
+
+	// ポート番号8080番
+	private final int PORT = 8080;
+
+	/** Driver */
+	private WebDriver driver = WebDriverUtils.webDriver;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,35 +49,107 @@ public class Case06 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:" + PORT + "/lms");
+		// タイトル 一致確認
+		assertEquals("ログイン | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("ログイン", driver.findElement(By.tagName("h2")).getText());
+		// ログインボタン 一致確認
+		assertEquals("ログイン", driver.findElement(By.className("btn-primary")).getAttribute("value"));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインIDの入力欄を空白にしておく
+		driver.findElement(By.name("loginId")).clear();
+
+		driver.findElement(By.name("loginId")).sendKeys("StudentAA01");
+		driver.findElement(By.name("password")).sendKeys("ItTest2026");
+		driver.findElement(By.className("btn-primary")).click();
+
+		pageLoadTimeout(50);
+		// タイトル 一致確認
+		assertEquals("コース詳細 | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("DEMOコース 2026年9月1日(火)～2026年9月30日(水)", driver.findElement(By.tagName("h2")).getText());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		// ドロップダウンメニュー「機能」をクリック
+		driver.findElement(By.className("dropdown-toggle")).click();
+		// その中からaタグの「ヘルプ」リンクをクリック
+		driver.findElement(By.linkText("ヘルプ")).click();
+
+		pageLoadTimeout(50);
+		// タイトル 一致確認
+		assertEquals("ヘルプ | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("ヘルプ", driver.findElement(By.tagName("h2")).getText());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		// 元のページのハンドルを記憶
+		String Handle = driver.getWindowHandle();
+		// その中からaタグの「よくある質問」リンクをクリック
+		driver.findElement(By.linkText("よくある質問")).click();
+
+		pageLoadTimeout(50);
+
+		//次のタブのハンドルを用意し、タブが新しく開かれていたらnewHandleに代入する
+		String newHandle = null;
+		for (String id : driver.getWindowHandles()) {
+			if (!id.equals(Handle)) {
+				newHandle = id;
+			}
+		}
+		//newHandleにハンドルを移す
+		driver.switchTo().window(newHandle);
+		// タイトル 一致確認
+		assertEquals("よくある質問 | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("よくある質問", driver.findElement(By.tagName("h2")).getText());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(5)
 	@DisplayName("テスト05 カテゴリ検索で該当カテゴリの検索結果だけ表示")
 	void test05() {
-		// TODO ここに追加
+
+		// 「【人材開発支援助成金】」リンクをクリック
+		driver.findElement(By.linkText("【人材開発支援助成金】")).click();
+
+		pageLoadTimeout(50);
+
+		List<WebElement> dtElements = driver.findElements(By.tagName("dt"));
+		// 検索結果 一致確認（1つめ）
+		WebElement dtElement1 = dtElements.get(0);
+		assertEquals("セルフ・キャリアドック制度とは何か", dtElement1.findElements(By.tagName("span")).get(1).getText());
+		// 検索結果 一致確認（2つめ）
+		WebElement dtElement2 = dtElements.get(1);
+		assertEquals("事業所が変わった場合、何かしら手続きをする必要がありますか？", dtElement2.findElements(By.tagName("span")).get(1).getText());
+		// 検索結果 一致確認（3つめ）
+		WebElement dtElement3 = dtElements.get(2);
+		assertEquals("助成金書類の作成方法が分かりません", dtElement3.findElements(By.tagName("span")).get(1).getText());
+		// 検索結果がスクショ範囲に収まるようにスクロール
+		scrollTo("300");
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
