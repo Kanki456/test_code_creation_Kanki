507b0084b305b5a591bc055c599ca5f267f611d4
KankiYuma <KankiYuma@192.168.3.136>
2026-09-25 17:00:54 +0900

    add:Case15.java 神吉雄真
    「Case15 受講生 初回ログイン 利用規約に不同意」の実装
    
    初回ログイン時に利用規約画面に遷移すること、利用規約画面で同意せずに「次へ」をクリックした際にエラーメッセージが表示されること　を確認

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f06_login2/Case15.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f06_login2;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能②
@@ -19,10 +26,22 @@ import org.junit.jupiter.api.TestMethodOrder;
 @DisplayName("ケース15 受講生 初回ログイン 利用規約に不同意")
 public class Case15 {
 
+	// ポート番号8080番
+	private final int PORT = 8080;
+
+	/** Driver */
+	private WebDriver driver = WebDriverUtils.webDriver;
+
 	/** 前処理 */
 	@BeforeAll
 	static void before() {
-		createDriver();
+		// パスワードに関するポップアップを回避するため、設定を追加
+		System.setProperty("webdriver.chrome.driver", "lib/chromedriver.exe");
+
+		ChromeOptions options = new ChromeOptions();
+		// シークレットモードで起動するオプションを追加
+		options.addArguments("--incognito");
+		webDriver = new ChromeDriver(options);
 	}
 
 	/** 後処理 */
@@ -35,21 +54,65 @@ public class Case15 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:" + PORT + "/lms");
+		// タイトル 一致確認
+		assertEquals("ログイン | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("ログイン", driver.findElement(By.tagName("h2")).getText());
+		// ログインボタン 一致確認
+		assertEquals("ログイン", driver.findElement(By.className("btn-primary")).getAttribute("value"));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに初期登録された未ログインの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		// ログインIDの入力欄を空白にしておく
+		driver.findElement(By.name("loginId")).clear();
+
+		driver.findElement(By.name("loginId")).sendKeys("StudentAA04");
+		driver.findElement(By.name("password")).sendKeys("StudentAA04");
+		driver.findElement(By.className("btn-primary")).click();
+
+		pageLoadTimeout(50);
+		// タイトル 一致確認
+		assertEquals("セキュリティ規約 | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("利用規約", driver.findElement(By.tagName("h2")).getText());
+		// 次へボタン 一致確認
+		assertEquals("次へ", driver.findElement(By.className("btn-primary")).getText());
+		getEvidence(new Object() {
+		}, "1");
+		// 検索結果がスクショ範囲に収まるようにスクロール
+		scrollTo("300");
+		getEvidence(new Object() {
+		}, "2");
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 「同意します」チェックボックスにチェックをせず「次へ」ボタンを押下")
 	void test03() {
-		// TODO ここに追加
+		scrollTo("200");
+		driver.findElement(By.className("btn-primary")).click();
+
+		pageLoadTimeout(50);
+		// タイトル 一致確認
+		assertEquals("セキュリティ規約 | LMS", driver.getTitle());
+		// h2タグ 一致確認
+		assertEquals("利用規約", driver.findElement(By.tagName("h2")).getText());
+		// エラーメッセージ 一致確認
+		assertEquals("セキュリティ規約への同意は必須です。", driver.findElement(By.className("error")).getText());
+		// 次へボタン 一致確認
+		assertEquals("次へ", driver.findElement(By.className("btn-primary")).getText());
+		getEvidence(new Object() {
+		}, "1");
+		// 検索結果がスクショ範囲に収まるようにスクロール
+		scrollTo("300");
+		getEvidence(new Object() {
+		}, "2");
 	}
 
 }
